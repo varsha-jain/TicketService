ea05152bfcba6c2d54815a08804608dd95c471fe
Varsha Jain <jain.vars@husky.neu.edu>
2018-05-29 04:14:52 -0400

    added unit test code

--- ticketservice/src/main/java/com/walmart/coding/challenge/App.java
+++ ticketservice/src/main/java/com/walmart/coding/challenge/App.java
@@ -15,13 +15,3 @@ public class App
     }
 }
 
-/*
-* to-do
-* 1. create menu and interaction in separate class --done
-* 2. validations --done
-* 3. while booking/reserving ask for row from user and scenarios while booking
-* 4. documentation ---done
-* 5. test
-* 6. update readme
-* 7. frequent commits --done
-* */
